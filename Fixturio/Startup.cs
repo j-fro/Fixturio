c87b65784242e4be536671d172d79c0dfb294ff8
Jacob Froman <jacob.h.froman@gmail.com>
2016-08-19 20:00:03 -0700

    Add project files.

---
+++ Fixturio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fixturio.Startup))]
+namespace Fixturio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
